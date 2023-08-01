9b431c42cb6c6c04b2d1aea3ef9f7d0c3fcb73c5
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-01 12:23:37 +0700

    method

--- tests/properties.test.ts
+++ tests/properties.test.ts
@@ -8,6 +8,10 @@ describe('Properties', () => {
             this.id = id;
             this.name = name;
         }
+
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
     }
     it('should can have properties', () => {
         const customer = new Customer(1, 'Ahmad Lutfi Rizki Patria');
@@ -17,5 +21,10 @@ describe('Properties', () => {
         console.info(customer.name);
         console.info(customer.age);
         console.info(customer);
+    });
+
+    it('should can have method', () => {
+        const customer: Customer = new Customer(1, 'Lutfi');
+        customer.sayHello('Laila');
     });
 });
