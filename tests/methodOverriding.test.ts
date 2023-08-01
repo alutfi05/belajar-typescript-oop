bbae158bd47cffc505f02c4fda61e315bb570045
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-01 15:31:16 +0700

    method overriding

---
+++ tests/methodOverriding.test.ts
@@ -0,0 +1,29 @@
+describe('Method Overriding', () => {
+    class Employee {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
+    }
+
+    class Manager extends Employee {
+        sayHello(name: string): void {
+            console.info(
+                `Hello ${name}, my name is ${this.name}, i am your manager`
+            );
+        }
+    }
+
+    it('should support', () => {
+        const employee: Employee = new Employee('Ahmad Lutfi');
+        employee.sayHello('Laila');
+
+        const manager: Manager = new Manager('Laila Indah Agustin');
+        manager.sayHello('Ahmad Lutfi');
+    });
+});
